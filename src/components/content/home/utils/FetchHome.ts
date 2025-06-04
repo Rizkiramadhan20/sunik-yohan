00c5fd612b8d72d20df81b7e85b5024223fe40eb
Rineta2 <rzkir571@gmail.com>
2025-06-04 21:02:06 +0700

    [update] home

--- src/components/content/home/utils/FetchHome.ts
+++ src/components/content/home/utils/FetchHome.ts
@@ -1,17 +1,16 @@
 import { homeProps } from "@/components/content/home/types/home";
 
+const API_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/${process.env.NEXT_PUBLIC_COLLECTIONS_HOME}`;
+
 export const fetchHomeContents = async (): Promise<homeProps[]> => {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/${process.env.NEXT_PUBLIC_COLLECTIONS_HOME}`,
-      {
-        next: { revalidate: 10 }, // Revalidate every 10 seconds
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key": process.env.NEXT_PUBLIC_API_KEY as string,
-        },
-      }
-    );
+    const response = await fetch(API_URL, {
+      next: { revalidate: 10 }, // Revalidate every 10 seconds
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.NEXT_PUBLIC_API_KEY || "",
+      },
+    });
 
     if (!response.ok) {
       throw new Error(`Failed to fetch home contents: ${response.statusText}`);
